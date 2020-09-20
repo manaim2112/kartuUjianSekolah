b0deae0eef9c9b6eb23e94bb6a42b497faebbc05
Muhammad Ainun Naim <49584629+manaim2112@users.noreply.github.com>
2020-09-20 23:53:22 +0700

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,164 +1,245 @@
-$(document).ready(function(){
+function html(Obj) {
+  Object.keys(Obj).forEach((a, b) =>  {
+    document.getElementById(a).innerHTML = Object.values(Obj)[b];
+  });
+}
 
-    let exelData = {};
-    let datalist = {
-      "title"     : "PEMERINTAH KABUPATEN PASURUAN <br> KEMENTERIAN AGAMA",
-      "subtitle"  : "Mts Sunan Ampel",
-      "alamat"    : "KarangAnyar Kraton Pasuruan",
-      "logo"      : "./dist/images/logo.png",
-      "cardname"  : "kartu Ujian Tengah Semester Genap",
-      "date"      : "Pasuruan, 09 Maret 2020",
-      "ttd"       : {
-          "title"     : "Kepala Sekolah",
-          "name"      : "IKHWAN, S.Ag",
-          "nip"       : ""
+let fileInput = document.getElementById("fileUploader"),
+    dragFile = document.querySelector('.upload'),
+    previewData = document.getElementById('seeData'),
+    createFile = document.getElementById('xlxsFile'),
+    saveFile = document.getElementById('saveFile');
+function handleFile(e) {
+  var files = e.target.files, f = files[0];
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var data = new Uint8Array(e.target.result);
+    var workbook = XLSX.read(data, {type: 'array'});
+
+    workbook.SheetNames.forEach((name)=>  {
+      let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[name]),
+          paperContent = '',
+          textContent = '',
+          dataArrayContent = [],
+          lengthRowObject = Math.floor(rowObject.length/8);
+      localStorage.setItem('ExcelData', JSON.stringify(rowObject));
+      rowObject.forEach((values, keys) =>  {
+        textContent += '<tr><td>' + values.No +'</td><td>' + values.no_ujian +'</td><td>'+ values.nama +'</td><td>'+ values.tanggal_lahir + '</td><td>'+ values.Ruang +'</td></tr>';
+        paperContent += 
+        `
+        <div class="table">
+          <table border="0" width="100%">
+            <tr style="text-transform: uppercase;">
+              <th width="100"><img src="`+ dataList.logo +`" width="60px" height="60px" alt="logos"></th>
+              <th>
+                <strong>
+                    <p>`+ dataList.title +`</p>
+                    <p>`+ dataList.subtitle +`</p>
+                </strong>
+                <small>`+ dataList.alamat +`</small>
+              </th>
+            </tr>
+            <tr>
+                <td colspan="2">
+                <hr>
+                  <p style="text-align: center;font-weight: bold; text-transform: uppercase;">`+ dataList.cardname +`</p>
+                </td>
+            </tr>
+            <tr>
+              <table border="0" style="width:100%;text-align:left;padding-left:5px;">
+                <tr> 
+                  <td> Nomer Ujian </td>
+                  <td>: ${values.no_ujian} </td>
+                </tr>
+                <tr> 
+                  <td> Nama </td>
+                  <td>: ${values.nama} </td>
+                </tr>
+                <tr> 
+                  <td> Tanggal Lahir </td>
+                  <td>: ${values.tanggal_lahir} </td>
+                </tr>
+                <tr> 
+                  <td> Ruang </td>
+                  <td>: ${values.Ruang} </td>
+                </tr>
+                <tr style="text-align:right;"> 
+                  <td>  </td>
+                  <td colspan="2">${dataList.date} </td>
+                </tr>
+                <tr> 
+                  <td>  </td>
+                  <td style="text-align:center;">${dataList.ttd.title} </td>
+                  <td>  </td>
+                </tr>
+                <tr> 
+                  <td>  </td>
+                  <td></td>
+                  <td>  </td>
+                </tr>
+                <tr> 
+                  <td>  </td>
+                  <td></td>
+                  <td>  </td>
+                </tr>
+                <tr> 
+                  <td>  </td>
+                  <td></td>
+                  <td>  </td>
+                </tr>
+                <tr> 
+                  <td>  </td>
+                  <td></td>
+                  <td>  </td>
+                </tr>
+                <tr> 
+                  <td>  </td>
+                  <td></td>
+                  <td>  </td>
+                </tr>
+                <tr> 
+                  <td>  </td>
+                  <td></td>
+                  <td>  </td>
+                </tr>
+                <tr> 
+                  <td>  </td>
+                  <td></td>
+                  <td>  </td>
+                </tr>
+                <tr> 
+                  <td>  </td>
+                  <td></td>
+                  <td>  </td>
+                </tr>
+                <tr> 
+                  <td>  </td>
+                  <td></td>
+                  <td>  </td>
+                </tr>
+                <tr> 
+                  <td>  </td>
+                  <td></td>
+                  <td>  </td>
+                </tr>
+                <tr> 
+                  <td>  </td>
+                  <td style="text-align:center;">${dataList.ttd.name} </td>
+                  <td>  </td>
+                </tr>
+                <tr> 
+                  <td>  </td>
+                  <td style="text-align:center;">NIP :  ${dataList.ttd.nip} </td>
+                  <td>  </td>
+                </tr>
+              </table>
+            </tr>
+          </table>
+        </div>`;
+        if((keys+1)%8 == 0) {
+            dataArrayContent.push(paperContent);
+            paperContent = '';
+        } else if((keys/8) >= lengthRowObject) {
+            if((keys+1) == rowObject.length)  {
+              dataArrayContent.push(paperContent);
+              paperContent = '';
+            }
+        }  
+      });
+      let div = '';
+      dataArrayContent.forEach((d)=>{
+        div +=  `<div class="pageBreak row" id="resultPage">${d}</div>`;
+      })
+      html({
+        page : div
+      });
+    });
+
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      onOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
       }
-    };
+    })
     
-    document.getElementById('title').innerHTML = datalist.title;
-    document.getElementById('subtitle').innerHTML = datalist.subtitle;
-    document.getElementById('alamat').innerHTML = datalist.alamat;
-    document.getElementById('logo').innerHTML = '<img src="'+ datalist.logo +'" width="60px" height="60px" alt="logos">';
-    document.getElementById('cardname').innerHTML = datalist.cardname;
-    document.getElementById('date').innerHTML = datalist.date;
-    document.getElementById('ttd-title').innerHTML = datalist.ttd.title;
-    document.getElementById('ttd-name').innerHTML = datalist.ttd.name;
-    document.getElementById('ttd-nip').innerHTML = datalist.ttd.nip;
-    
-  $("#fileUploader").change(function(evt){
-        var selectedFile = evt.target.files[0];
-        var reader = new FileReader();
-        reader.onload = function(event) {
-          var data = event.target.result;
-          var workbook = XLSX.read(data, {
-              type: 'binary'
-          });
-          workbook.SheetNames.forEach(function(sheetName) {
-            
-              var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-              var json_object = JSON.stringify(XL_row_object);
-              //document.getElementById("jsonObject").innerHTML = json_object;
-            
-                exelData = JSON.parse(json_object);
-                let textContent = '';
-                let paperContent = '';
-                
-                for(var i=0; i < exelData.length; i++)
-                {
-                    /**
-                     * Datatable List
-                     */
-                    textContent += '<tr><td>' + exelData[i].No +'</td><td>' + exelData[i].no_ujian +'</td><td>'+ exelData[i].nama +'</td><td>'+ exelData[i].tanggal_lahir + '</td><td>'+ exelData[i].Ruang +'</td></tr>';
-                    
-                    /**
-                     * Data PaperContent
-                     */
-                    paperContent += `
-                        <div class="table">
-                          <table border="0">
-                            <tr style="text-transform: uppercase;">
-                              <th width="100"><img src="`+ datalist.logo +`" width="60px" height="60px" alt="logos"></th>
-                              <th>
-                                <strong>
-                                    <p>`+ datalist.title +`</p>
-                                    <p>`+ datalist.subtitle +`</p>
-                                </strong>
-                                <small>`+ datalist.alamat +`</small>
-                              </th>
-                            </tr>
-                          </table>
-                          <hr>
-                          <div class="content">
-                            <p style="text-align: center;font-weight: bold; text-transform: uppercase;">`+ datalist.cardname +`</p>
-                            <div class="row-3 text-left">
-                              <div class="col-1">
-                                <p>Nomer Ujian</p>
-                                <p>Nama</p>
-                                <p>Tanggal Lahir</p>
-                                <p>Ruang</p>
-                              </div>
-                              <div class="col-2">
-                                <p>: `+ exelData[i].no_ujian +`</p>
-                                <p>: `+ exelData[i].nama +`</p>
-                                <p>: `+ exelData[i].tanggal_lahir +`</p>
-                                <p>: `+ exelData[i].Ruang +`</p>
-                                <div class="foot" style="text-align: center;">
-                                  <p>`+ datalist.date +`</p>
-                                  <p>`+ datalist.ttd.title +`</p>
-                                  <br><small>TTD.</small>
-                                  <p>`+ datalist.ttd.name +`</p>
-                                  <p> NIP : `+ datalist.ttd.nip +`</p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>`;
-                    // Page Break
-                    if((i+1)%8 == 0) {
-                      paperContent += '<span id="nextpage"></span>';
-                    }
+    Toast.fire({
+      icon: 'success',
+      title: 'Import Berhasil'
+    })
+    document.querySelector('.upload').style.display = 'none';
+    document.querySelector('.container').style =  "position:fixed;";
+  };
+  reader.readAsArrayBuffer(f);
+}
+fileInput.addEventListener('change', handleFile, false);
+dragFile.addEventListener('drop', handleFile, false);
 
-                }
-                document.getElementById("dataJson").innerHTML = textContent;
-                document.getElementById("lengthJSON").innerHTML = exelData.length;
-                document.getElementById("resultPage").innerHTML = paperContent;
-            });
-        };
+previewData.addEventListener('click', (e) => {
+  let data = JSON.parse(localStorage.getItem('ExcelData')),
+      textContent = '';
 
-        reader.onerror = function(event) {
-          console.error("File could not be read! Code " + event.target.error.code);
-        };
-
-        reader.readAsBinaryString(selectedFile);
-        
+  data.forEach((value)=> {
+    textContent += `
+      <tr>
+        <td> ${value.No} </td>
+        <td> ${value.no_ujian} </td>
+        <td> ${value.nama} </td>
+        <td> ${value.tanggal_lahir} </td>
+        <td> ${value.Ruang} </td>
+      </tr>
+    `;
+  })
+  Swal.fire({
+    title : 'Data List',
+    width : 800,
+    html : `
+    <table border="1" style="width: 100%;display: block; border:#000 2px;">
+      <thead>
+          <td> No </td>
+          <td> Nomer Ujian </td>
+          <td> Nama </td>
+          <td> Tanggal Lahir </td>
+          <td> Ruang </td>
+      </thead>
+      <tbody>
+        ${textContent}
+      </tbody>
+      <tfoot>
+          Jumlah Total : <strong> ${data.length} </strong> siswa
+      </tfoot>
+    </table>`
   });
+})
 
-  /**
-   * Create .xlsx file with data template
-   */
-  /**
-   * Create .xlsx file with data template
-   */
-  // let tempXLSX = document.querySelector('#downloadtemplate');
-  // tempXLSX.addEventListener('click', (event) => {
+createFile.addEventListener('click', (e) => {
+  var wb = XLSX.utils.book_new();
+  var ws_name = "SheetJS";
 
-  //   // Create workbook
-  //   let wb = XLSX.utils.book_new();
-  //   // Update properties
-  //   wb.Props = {
-  //     Title : "Aplikasi Kartu Ujian Sekolah",
-  //     Subject : "Vol.01",
-  //     Author : "@manaim2112",
-  //     CreatedDate : new Date(2020,12,21)
-  //   };
-  //   // SheetName
-  //   wb.SheetNames.push("Sheet 1");
-  //   // Content Data
-  //   let ws_data = [
-  //     ['hello', 'world']
-  //   ];
-  //   let ws = XLSX.utils.aoa_to_sheet(ws_data);
-  //   wb.Sheets["Test Sheet!"] = ws;
-  //   // download xlsx
-  //   let wbout = XLSX.write(wb, {
-  //     bookType : 'xlsx',
-  //     type : 'binary'
-  //   });
-  // });
+  /* make worksheet */
+  var ws_data = [
+    [ "No", "no_ujian", "nama", "tanggal_lahir", "Ruang"],
+    [  1 ,  '0021-123-202' ,  'Your Name' ,  "-" ,  5 ],
+    [  2 ,  '0021-123-203' ,  'Your Name HUH' ,  "-" ,  5 ],
+  ];
+  var ws = XLSX.utils.aoa_to_sheet(ws_data);
 
-  document.querySelector("#btnPrint").addEventListener('click', (event) => {
-    // Get the element.
-    var element = document.getElementById('page');
+  /* Add the worksheet to the workbook */
+  XLSX.utils.book_append_sheet(wb, ws, ws_name);
+  /* bookType can be any supported output type */
+  var wopts = { bookType:'xlsx', bookSST:false, type:'array' };
 
-    // Generate the PDF.
-    html2pdf().from(element).set({
-      margin: 0.2,
-      filename: 'download.pdf',
-      html2canvas: { scale: 1 },
-      image : {type: 'jpeg', quality:0.98},
-      jsPDF: {orientation: 'portrait', unit: 'in', format: 'legal', compressPDF: false},
-      pagebreak : { mode :'css', before: '#nextpage'}
+  var wbout = XLSX.write(wb,wopts);
 
-    }).save();
-  });
-});
+  /* the saveAs call downloads a file on the local machine */
+  saveAs(new Blob([wbout],{type:"application/octet-stream"}), "template.xlsx");
+
+})
+
+saveFile.addEventListener('click', (e) =>  {
+    window.print();
+})
