14fe2040460910e9cbc5641520bf33ee945d6532
Muhammad Ainun Naim <49584629+manaim2112@users.noreply.github.com>
2020-09-21 00:04:12 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,13 @@
+let dataList = {
+    "title"     : "PEMERINTAH KABUPATEN PASURUAN <br> KEMENTERIAN AGAMA",
+    "subtitle"  : "Mts Sunan Ampel",
+    "alamat"    : "KarangAnyar Kraton Pasuruan",
+    "logo"      : "./logo.png",
+    "cardname"  : "kartu Ujian Tengah Semester Ganjil",
+    "date"      : "Pasuruan, 26 September 2020",
+    "ttd"       : {
+        "title"     : "Kepala Sekolah",
+        "name"      : "IKHWAN, S.Ag",
+        "nip"       : ""
+    }
+};
